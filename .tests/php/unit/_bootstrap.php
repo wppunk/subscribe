b7e038bc4bc74d2d8c1c774aee56867f9ce0ef26
Maxym Denysenko <mdenisenko95@gmail.com>
2020-12-20 22:54:11 +0200

    Add test environment

---
+++ .tests/php/unit/_bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Bootstrap file for unit tests that run before all tests.
+ *
+ * @since   {VERSION}
+ * @link    {URL}
+ * @license GPLv2 or later
+ * @package PluginName
+ * @author  {AUTHOR}
+ */
+
+define( 'SUBSCRIBE_VERSION', '1.0.0' );
+define( 'SUBSCRIBE_PATH', realpath( __DIR__ . '/../../../' ) . '/' );
+define( 'SUBSCRIBE_URL', 'https://site.com/wp-content/plugins/subscribe/' );
+define( 'ABSPATH', realpath( PLUGIN_NAME_PATH . '../../' ) . '/' );
